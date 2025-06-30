f38aa6791a268ce0babd0ebaf1a0e19336c0a388
Sri Renuka Shambavi <srirenu88@gmail.com>
2025-06-30 20:14:16 +0530

    Delete ownerRoutes.js

--- ownerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addProperty, getOwnerProperties } = require('../controllers/ownerController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/add-property', authMiddleware, addProperty);
-router.get('/my-properties', authMiddleware, getOwnerProperties);
-
-module.exports = router;
