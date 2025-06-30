ac60da4507ed0527b36d684e87bacb9e65b3e7db
Sri Renuka Shambavi <srirenu88@gmail.com>
2025-06-30 20:14:03 +0530

    Delete adminRoutes.js

--- adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllUsers, getAllProperties, getAllBookings } = require('../controllers/adminController');
-const router = express.Router();
-
-router.get('/users', getAllUsers);
-router.get('/properties', getAllProperties);
-router.get('/bookings', getAllBookings);
-
-module.exports = router;
